74886d8cfb08d70d16c56895144d4231ecd77d54
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-26 15:50:03 +0100

    Contemplate the case of getting back the main triangle... by avoiding it

--- __test__/Sierpinski.test.js
+++ __test__/Sierpinski.test.js
@@ -2,6 +2,7 @@ import {testSubject} from '../src/Sierpinski'
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min
+const MIN = 100  // To make sure we don’t get back just the main triangle
 const MAX = 1000
 
 const testRuns = 100
@@ -9,9 +10,10 @@ const testRuns = 100
 describe(`Sierpinski triangle`, () => {
   test(`has a multiple of 3 triangles`, () => {
     for (let i = 0 ; i < testRuns ; i++) {
-      expect(
-        testSubject(getRandomArbitrary(0, MAX), getRandomArbitrary(0, MAX), getRandomArbitrary(0, MAX)).length % 3)
-        .toBe(0)
+      const results =
+        testSubject(getRandomArbitrary(MIN, MAX), getRandomArbitrary(MIN, MAX), getRandomArbitrary(MIN, MAX))
+
+      expect(results.length % 3).toBe(0)
     }
   })
 })
